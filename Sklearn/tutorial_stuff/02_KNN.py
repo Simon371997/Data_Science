582ee097ad6ea1442a07335c63cea5b0dbdd5f1c
Simon Henes <simon.henes@live.de>
2023-10-26 14:42:36 +0200

    Update 02_KNN.py

--- Sklearn/tutorial_stuff/02_KNN.py
+++ Sklearn/tutorial_stuff/02_KNN.py
@@ -1,12 +1,24 @@
+# Infos about KNN
+# kann supervied und unsupervised verwendet werden
+# empfindlich gegenüber Ausreißern, vergleichsweise rechenintensiv
+# n_neigbours(k) muss sinnvoll gewählt werden:
+# Im Trainig merkt sich der Algo alle vorhandenen Datenpunkte, 
+# um bei der Vorhersage die 'k' nächsten Nachbarn zu finden um eine Klasse zuzuordnen
+
+
+
+
 import pandas as pd
 import numpy as np
 from sklearn import neighbors, metrics
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-data = pd.read_csv('./tutorial_data/CAR_EVALUATION.csv')
-print(data.head())
+data = pd.read_csv('./Sklearn/tutorial_stuff/tutorial_data/CAR_EVALUATION.csv')
+
 
 X = data[['buying', 'maint', 'safety']].values
 y = data[['Target']]
@@ -23,5 +35,24 @@ label_maping = {
 }
 y['Target'] = y['Target'].map(label_maping)
 y = np.array(y)
-print(y)
+
+# create Model
+knn = neighbors.KNeighborsClassifier(n_neighbors=25, weights='uniform')
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+knn.fit(X_train, y_train)
+
+predictions = knn.predict(X_test)
+
+accuracy = metrics.accuracy_score(y_test, predictions)
+
+print(f'Predictions: {predictions}')
+print(f'Accuracy: {accuracy}')
+
+a = 1727
+print("actual value ", y[a])
+print("predicted value", knn.predict(X)[a])
+
+
+
 
